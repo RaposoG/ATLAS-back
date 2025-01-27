510c45f95817644f04e95195931325edc717e2e5
Gabriel Raposo <gabriel.henrique7@hotmail.com>
2025-01-26 23:38:45 -0300

    feat(server): adicionar exibição de endereços de rede
    - Importar o módulo 'os' para acessar informações de rede.
    - Criar a função 'getNetworkAddresses' para obter endereços IPv4 não internos.
    - Modificar a configuração do logger para usar '===' em vez de '=='.
    - Exibir endereços de rede disponíveis no console ao iniciar o servidor.
    
    Essas alterações melhoram a visibilidade do servidor, permitindo que os desenvolvedores vejam facilmente os endereços de rede disponíveis, facilitando o acesso e a depuração durante o desenvolvimento.

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import { errorHandler } from "./error-handler";
 import { join } from "node:path";
 import { readFileSync } from "node:fs";
 import { discordAuthRoutes } from "./routes/auth/discord-auth-routes";
+import os from "os";
 
 const _logo = join(__dirname, "assets", "logoExemplo.png");
 const _favIcon = join(__dirname, "assets", "logoExemplo.png");
@@ -17,7 +18,7 @@ const logo = readFileSync(_logo);
 const favIcon = readFileSync(_favIcon);
 
 const app = fastify({
-  logger: env.NODE_ENV == "dev" ? true : false,
+  logger: env.NODE_ENV === "dev",
 }).withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
@@ -77,10 +78,33 @@ app.register(fastifyCors);
 //Auth
 app.register(discordAuthRoutes);
 
+function getNetworkAddresses() {
+  const interfaces = os.networkInterfaces();
+  const addresses: { name: string; address: string }[] = [];
+
+  for (const name in interfaces) {
+    for (const iface of interfaces[name] || []) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        addresses.push({ name, address: iface.address });
+      }
+    }
+  }
+
+  return addresses;
+}
+
 app
   .listen({ port: env.PORT, host: "0.0.0.0" })
   .then(() => {
     console.log(`Server is running on port ${env.PORT}`);
+    const networkAddresses = getNetworkAddresses();
+
+    console.log("Server is available at the following addresses:");
+    networkAddresses.forEach(({ name, address }) => {
+      console.log(`- ${name}: http://${address}:${env.PORT}`);
+    });
+
+    console.log(`Local: http://localhost:${env.PORT}`);
   })
   .catch((error) => {
     console.error("Error starting server:", error);
