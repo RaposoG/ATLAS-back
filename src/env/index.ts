45bf90e0a4561686b94ac244ea32ae9abefa72a7
Gabriel Raposo <gabriel.henrique7@hotmail.com>
2025-02-14 00:38:35 -0300

    feat(env): adicionar variáveis de ambiente para integração com Discord
    - Adicionadas as variáveis de ambiente DATABASE_URL, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_TOKEN_BOX e DISCORD_GUILD_ID no arquivo src/env/index.ts.
    - Essas variáveis são essenciais para a configuração e autenticação do bot no Discord.
    
    Essas alterações melhoram a configuração do ambiente, permitindo que o bot se conecte e interaja com a API do Discord de forma segura e eficiente, facilitando a implementação de novas funcionalidades relacionadas ao Discord.

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,11 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   PORT: z.coerce.number().default(3434),
   SECRET_JWT: z.string(),
+  DATABASE_URL: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
+  DISCORD_TOKEN_BOX: z.string(),
+  DISCORD_GUILD_ID: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
